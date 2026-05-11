9b939d98d1be21b3477c60fb5d4e1277681b11c7
dlwnsgur4378 <dlwnsgur4378@gmail.com>
2026-05-11 15:14:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import gradio as gr
+from ultralytics import YOLO
 from PIL import Image
 import numpy as np
 
-def predict(img):
-    return img
+# 모델 불러오기
+model = YOLO("best_emotion_model.pt")
 
+# 예측 함수
+def predict(img):
+
+    results = model(img)
+
+    # 결과 이미지 생성
+    plotted = results[0].plot()
+
+    return plotted
+
+# 웹 인터페이스
 demo = gr.Interface(
     fn=predict,
     inputs=gr.Image(type="pil"),
     outputs=gr.Image(),
-    title="Dog Emotion AI"
+    title="강아지 감정 분석 AI"
 )
 
 demo.launch(server_name="0.0.0.0", server_port=7860)
